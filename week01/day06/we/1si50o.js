3780792b1d5e016e16fa2ff772315cb55b24a635
enter your name <shivamsingh4458@gmail.com>
2021-04-30 16:17:18 +0530

    week01-day06-we file

---
+++ week01/day06/we/1si50o.js
@@ -0,0 +1,22 @@
+function runProgram(input) {
+    var input1 = Number(input);
+    var over = 50;
+    console.log(over * input1);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "")
+    runProgram(read);
+});
+
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "")
+    runProgram(read);
+    process.exit(0);
+});
