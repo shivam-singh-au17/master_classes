7605a06e8085a22be4a7e82a53d3ef3fb1ff1f40
NOOR <shivamsingh4458@gmail.com>
2021-06-21 09:55:00 +0530

    initial commit

--- Extra_Practice/Revision/week02OJq/g.js
+++
@@ -1,4 +0,0 @@
-var num = 3;
-for (var i = 1; i <= 10; i++){
-    console.log(`The square of ${i} is ${i*i}`);
-}
--- Extra_Practice/Revision/week02OJq/j.js
+++
@@ -1,6 +0,0 @@
-var num = 6;
-var result = 1;
-for (var i = 1; i <= num; i++){
-    result *= i
-}
-console.log(result);
