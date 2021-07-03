e85401a714aadc1e1432486766ecf0dea505bdb5
NOOR <shivamsingh4458@gmail.com>
2021-07-03 18:45:23 +0530

    unit02-week09

---
+++ UNIT-02/week09/day02/assignment/SumofDigits.js
@@ -0,0 +1,51 @@
+
+
+function findSum(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+function runProgram(input) {
+
+    var Input = input.split("\n");
+    var testCases = Number(Input[0]);
+
+    for (var i = 1; i <= testCases; i++) {
+        var arr = Input[i * 2].split("").map(Number);
+    
+        var ans = findSum(arr);
+        console.log(ans);
+
+    }
+
+}
+
+if (process.env.USERNAME === "shiva") {
+    runProgram(`2
+5
+12 14 16 17 29
+6
+1 2 3 4 5 51`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    var read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
+
