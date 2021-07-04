5c540262396416c47a997a70330eef9b66ae0997
NOOR <shivamsingh4458@gmail.com>
2021-07-04 20:22:03 +0530

    2021-05-06-all dsa-file

---
+++ PRACTICE/NJ-Batch/UNIT02/All-DSA-Questions/2021-05-06/1NoddyExamination.js
@@ -0,0 +1,81 @@
+
+function findMaxCountSolQ(arr, k) {
+    var count = 0;
+    var skip = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] <= k && skip < 2) {
+            count++;
+        } else {
+            skip++;
+        }
+    }
+    return count;
+}
+
+function runProgram(input) {
+
+    var Input = input.split("\n");
+
+    var lenAndK = Input[0].split(" ").map(Number);
+    var k = lenAndK[1];
+    console.log('k:', k)
+
+    var arr = Input[1].split(" ").map(Number);
+    console.log('arr:', arr)
+
+    var ans = findMaxCountSolQ(arr, k);
+    console.log(ans);
+
+}
+
+if (process.env.USERNAME === "shiva") {
+    runProgram(`7 6
+7 4 5 7 4 2 2`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    var read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
+
+
+
+
+
+
+
+
+// 2nd mathod 
+
+
+
+
+// function findMaxCountSolQ(items, k) {
+//     var count = 0;
+//     for (var i = 0; i < items.length; i++) {
+//         if (items[i] > k) {
+//             count++;
+//         }
+//         if (count == 2) {
+//             return i - 1;
+//         }
+//     }
+//     if (count == 1) {
+//         return items.length - 1
+//     } else if (count < 2) {
+//         return items.length
+//     }
+
+// }
---
+++ PRACTICE/NJ-Batch/UNIT02/All-DSA-Questions/2021-05-06/2CodingStreak.js
@@ -0,0 +1,72 @@
+
+function sumArrStr(str) {
+    var temp = "";
+    for (var i = 0; i < str.length; i++) {
+        temp += str[i]
+    }
+    return temp;
+}
+
+function countCodingIn_1_Days(str) {
+    var count = 1;
+    var temp = str[0]
+    var res = 0
+    for (var i = 1; i <= str.length; i++) {
+        if (str[i] == temp && str[i] == 'C') {
+            count++
+        } else {
+            res = Math.max(res, count);
+            count = 1;
+            temp = str[i]
+        }
+    }
+    return res;
+}
+
+function maxCodingCount(strArr) {
+    var nDaysCoding = countCodingIn_1_Days(sumArrStr(strArr))
+    var oneDayCoding = 0
+    for (var i = 0; i < strArr.length; i++) {
+        oneDayCoding = Math.max(countCodingIn_1_Days(strArr[i]), oneDayCoding);
+    }
+    console.log(oneDayCoding, nDaysCoding);
+}
+
+
+function runProgram(input) {
+
+    var Input = input.split("\n");
+    var testCases = Number(Input[0]);
+
+    var strArr = []
+    for (var i = 1; i <= testCases; i++) {
+        strArr.push(Input[i]);
+    }
+    maxCodingCount(strArr)
+
+}
+
+if (process.env.USERNAME === "shiva") {
+    runProgram(`4
+SSSSEEEECCCCEECCCC
+CCCCCSSSSEEECCCCSS
+SSSSSEEESSCCCCCCCS
+EESSSSCCCCCCSSEEEE`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    var read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
