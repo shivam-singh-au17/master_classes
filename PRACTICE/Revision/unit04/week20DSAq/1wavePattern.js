03ee33d7370ae97066cd7cd6cff5570021e4bf1e
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-20 08:38:46 +0530

    dsa file

--- PRACTICE/Revision/unit04/week20DSAq/1wavePattern.js
+++ PRACTICE/Revision/unit04/week20DSAq/1wavePattern.js
@@ -1,2 +1,28 @@
 
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
+
+var checkWavePattern = function (head) {
+
+    let curnt = head;
+    if (head.next == null) {
+        return true;
+    }
+    let i = 0;
+    let temp = false;
+    while (curnt.next != null) {
+        if (i % 2 == 1 && curnt < curnt.next && curnt.next > curnt.next.next) {
+            temp = true
+        } else if (i % 2 == 0 && curnt > curnt.next && curnt.next < curnt.next.next) {
+            temp = true
+        }
+        curnt = curnt.next
+        i++;
+    }
+    return temp;
+};
