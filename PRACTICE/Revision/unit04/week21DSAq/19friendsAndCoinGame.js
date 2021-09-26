8e5c09ff8622a83eafd6f3580cbb11b4dd0650b1
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-26 16:57:51 +0530

    work files

---
+++ PRACTICE/Revision/unit04/week21DSAq/19friendsAndCoinGame.js
@@ -0,0 +1,49 @@
+
+
+let CoinsGame = function (arr) {
+
+    let n = arr.length;
+    let res = 0;
+
+    for (let i = n / 3; i < n; i += 2) {
+        res += arr[i];
+    }
+
+    return res;
+
+};
+
+
+function runProgram(input) {
+
+    let newData = input.split("\n");
+    let arr = newData[1].trim().split(" ").map(Number);
+    arr.sort((a, b) => { return a - b });
+    let ans = CoinsGame(arr);
+    console.log(ans);
+
+}
+
+
+if (process.env.USERNAME === "Jay Mahakaal") {
+    runProgram(`6
+2 4 1 3 5 6`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
+
