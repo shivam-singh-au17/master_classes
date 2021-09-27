c2ae26ead9d90e7f4220e6403bace2194ad0c757
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 09:22:32 +0530

    dsa q

---
+++ PRACTICE/Revision/unit04/week21DSAq/16collect_I.js
@@ -0,0 +1,21 @@
+//       #include < iostream >
+//       #include < vector >
+
+//           using namespace std;
+
+//       int main(int argc, char const * argv[])
+//       {
+//           int n;
+//           cin >> n;
+//           vector < int > v(n + 1), pos(n + 1);
+//           for (int i = 1; i <= n; ++i) {
+//               cin >> v[i];
+//               pos[v[i]] = i;
+//           }
+//           int total_rounds = 1; // we'll always need at least one round because the input sequence will never be empty
+//           for (int i = 2; i <= n; ++i) {
+//               if (pos[i] < pos[i - 1]) total_rounds++;
+//           }
+//           cout << total_rounds << '\n';
+//           return 0;
+//       }
