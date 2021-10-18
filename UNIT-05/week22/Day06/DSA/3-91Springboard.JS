a1f223ec61eac6fc5c97b14e00c67de92666d058
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-18 10:32:39 +0530

    adding some dsa probleam

---
+++ UNIT-05/week22/Day06/DSA/3-91Springboard.JS
@@ -0,0 +1,26 @@
+
+function runProgram(input) {
+
+}
+
+
+if (process.env.USERNAME === "Jay Mahakaal") {
+    runProgram(``);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    var read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
