61cbe9524bc68179ce6a631be34ca575c8d7d7fc
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-27 11:57:58 +0530

    dsa files

---
+++ ThemedContest/ThemedContest_01/5Battle of Odd & Even.js
@@ -0,0 +1,22 @@
+function battleOfOddAndEven(n, arr) {
+
+    let oddSum = 0;
+    let evenSum = 0;
+    for (let i = 0; i < n; i++) {
+        if (arr[i] % 2 == 1) {
+            oddSum += arr[i]
+        } else {
+            evenSum += arr[i]
+        }
+    }
+
+    if (oddSum > evenSum) {
+        console.log("Odd");
+    } else {
+        console.log("Even");
+    }
+
+}
+
+
+
