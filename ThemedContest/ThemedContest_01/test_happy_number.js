0797755190e0c9ed8ad1cc8e461e6bd3960313ce
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-27 11:58:15 +0530

    dsa files

---
+++ ThemedContest/ThemedContest_01/test_happy_number.js
@@ -0,0 +1,41 @@
+
+function addTwoNum(num) {
+    let sqr = 0;
+    while (num != 0) {
+        let digit = num % 10;
+        sqr += digit * digit;
+        num = parseInt(num / 10, 10);
+    }
+    return sqr;
+}
+
+
+function findBeautiful(num) {
+
+    while (true) {
+
+        if (num == 1) {
+            return true;
+        }
+        num = addTwoNum(num);
+        if (num == 4) {
+            return false;
+        }
+    }
+
+}
+
+function beautifulNumber(L1, L2) {
+    let sum = 0;
+    for (let i = L1; i <= L2; i++) {
+        if (findBeautiful(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+
+console.log(beautifulNumber(7, 7) );
+
+
