898b6e729fddac46e6ea020d55ea8bb32b058fee
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-12 18:05:18 +0530

    Adding Practice File

---
+++ UNIT-05/week24/Day02/coding/FileUploads/src/utils/gallery-uplode.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, "../gallery"));
+    },
+    filename: function (req, file, callback) {
+        const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        callback(null, uniquePrefix + "-" + file.originalname);
+    },
+});
+
+const fileFilter = function (req, file, callback) {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
+        callback(null, true)
+    } else {
+        callback(null, false)
+    }
+}
+
+const galleryUplode = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter,
+})
+
+module.exports = galleryUplode;
+
