abf80009196a4535d991e26202ef0c9702f677f8
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-12 17:57:14 +0530

    Adding Practice File

--- PRACTICE/Revision/unit03/week13DSAq/11ImplementQueueUsingStack.js
+++ PRACTICE/Revision/unit03/week13DSAq/11ImplementQueueUsingStack.js
@@ -6,24 +6,17 @@ class Queue {
     }
     push(value) {
         this.s1.push(value);
-
     }
     pop() {
         while (this.s1.stack.length > 1) {
             this.s2.push(this.s1.stack.pop());
         }
-
-
         var temp = this.s1.pop();
-
         this.s2.stack.reverse();
         [this.s1.stack, this.s2.stack] = [this.s2.stack, this.s1.stack]
-
-
     }
     front() {
         return this.s1.stack[0];
-
     }
     isEmpty() {
         if (this.s1.stack.length == 0)
