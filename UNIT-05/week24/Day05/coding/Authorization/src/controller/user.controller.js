908aa80b1b11e6c2341fab13b0cccfa8e75c8e86
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-12 18:09:11 +0530

    Adding Practice File

---
+++ UNIT-05/week24/Day05/coding/Authorization/src/controller/user.controller.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user.model");
+
+const protect = require("../middlewares/protect")
+
+router.get("/", protect, async (req, res) => {
+    try {
+        const users = await User.find({}).select("-password").lean().exec();
+        // const user = await User.find({}).lean().exec();
+        return res.status(200).json({ data: user });
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+})
+
+
+module.exports = router;
